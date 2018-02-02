e8e0d7f8425ac0d62ce2b3951aa2b179e4df3097
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-02 15:49:50 +0530

    Migration and stuff

---
+++ models/user.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var user = sequelize.define('user', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return user;
+};
